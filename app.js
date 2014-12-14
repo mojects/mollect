2a9fb3e8094f5d616b9096eb9813b86695d4e539
Dan <dan@AVODNs-MacBook-Pro.local>
2014-12-14 00:54:03 -0600

    2

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-winston.log('error', "OK");
+winston = require('winston');
+winston.info('Starting');
 
-var http = require('http');
+var http = require('http'),
+    fs = require('fs'),
+    mime = require('mime');;
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.end('Hello Http');
-});
-server.listen(8181);
+http.createServer(function(request, response) {
+    winston.info(request.url);
+    if (request.url == "/")
+        outputFile('www/index.html', response);
+    else
+        outputFile('www' + request.url, response);
+}).listen(8181);
+
+
+function outputFile(path, response) {
+
+    fs.readFile(path, function (err, html) {
+        if (err) {
+            outputError("err", response);
+            return;
+        }
+        response.writeHeader(200, {"Content-Type": mime.lookup(path)});
+        response.write(html);
+        response.end();
+    });
+}
+
+
+
+function outputError(err, response) {
+    response.writeHeader(500, {"Content-Type": "text/html"});
+    response.write(err);
+    response.end();
+}
