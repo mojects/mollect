7b2d6450b4f3ca716da059dd797842f7286c1ff6
dan <dan@coav.ru>
2014-12-22 18:17:35 -0600

    test

--- www/js/controllers/delete.js
+++ www/js/controllers/delete.js
@@ -3,6 +3,7 @@ angular.module('mollect')
 .controller('DeleteCtrl', function($scope, Node, $routeParams) {
     console.log('DeleteCtrl');
 
+    
         var node = new Node($routeParams.nodeId);
         node.delete().then(function(){
             location.href = "#/";
